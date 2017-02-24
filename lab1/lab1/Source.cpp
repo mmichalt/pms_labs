37a1682987e0584ab2a884f9118f92f1c4bbd6d7
Michal Matviychuk <michalmatviychuk@gmail.com>
2017-02-24 13:15:34 +0200

    no message

--- lab1/lab1/Source.cpp
+++ lab1/lab1/Source.cpp
@@ -2,6 +2,8 @@
 #include <windows.h>
 #include <GL/glut.h> 
 #include <iostream>
+#include <vector>
+using namespace std;
 void Initialize()
 {
 	glClearColor(0, 0, 0, 1);
@@ -12,47 +14,68 @@ void Initialize()
 bool night = false;
 int counter = 0;
 double x = -0.05, y = -0.05;
+float previousx=0.0, previousy=0.0;
+vector<float>xs;
+vector<float>ys;
+bool clicked = false;
+void OnMouseClick(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		clicked = true;
+	}
+}
 void Timer(int k)
 {
 	glutPostRedisplay();
 	glutTimerFunc(50, Timer, 0);
 	counter++;
-	if (y < 0.95)
-	{
-		if (x > 0.95)
-		{
-			x = -0.05;
-			y += 0.1;
-		}
-		else
-			x += 0.1;
-	}
-	else {
-		night = true;
-		y = -0.05;
-	}
+		x = (double)rand() / RAND_MAX;
+		y = (double)rand() / RAND_MAX;
+		xs.push_back(x);
+		ys.push_back(y);
 }
+int i = 0;
 void Draw()
 {
-	glColor3f(1.0, 1.0, 1.0); 
+
+	glColor3f(1.0, 1.0, 1.0);
 	glPointSize(4);
-	glBegin(GL_LINE_STRIP);
-	if(night)
-		glColor3f(0.0, 0.0,0.0);
+	if (clicked)
+	{
+		
+			glBegin(GL_LINE_STRIP);
+			glColor3f(0.0, 0.0, 0.0);
+			glVertex2d(xs[i], ys[i]);
+			glVertex2d(xs[i] - .01, ys[i] - .01);
+			glVertex2d(xs[i], ys[i]);
+			glVertex2d(xs[i] - .01, ys[i] + .01);
+			glVertex2d(xs[i], ys[i]);
+			glVertex2d(xs[i] + .01, ys[i] - .01);
+			glVertex2d(xs[i], ys[i]);
+			glVertex2d(xs[i] + .01, ys[i] + .01);
+			glVertex2d(xs[i], ys[i]);
+			glEnd();
+			i++;
+	}
 	else
+	{
+		glBegin(GL_LINE_STRIP);
 		glColor3f((double)rand() / (RAND_MAX), (double)rand() / (RAND_MAX), (double)rand() / (RAND_MAX));
-	glVertex2d(x, y);
-	glVertex2d(x - .01, y - .01);
-	glVertex2d(x, y);
-	glVertex2d(x - .01, y + .01);
-	glVertex2d(x, y);
-	glVertex2d(x + .01, y - .01);
-	glVertex2d(x, y);
-	glVertex2d(x + .01, y + .01);
-	glVertex2d(x, y);
-	glEnd();
+		glVertex2d(x, y);
+		glVertex2d(x - .01, y - .01);
+		glVertex2d(x, y);
+		glVertex2d(x - .01, y + .01);
+		glVertex2d(x, y);
+		glVertex2d(x + .01, y - .01);
+		glVertex2d(x, y);
+		glVertex2d(x + .01, y + .01);
+		glVertex2d(x, y);
+		glEnd();
+	}
 	glFlush();
 }
+
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
@@ -61,9 +84,9 @@ int main(int argc, char **argv)
 	glutInitWindowPosition(100, 100);	
 	glutCreateWindow("SKY");		
 	Initialize();			
-	glClearColor(1, 1, 1, 1);
 	glutDisplayFunc(Draw);	
 	glutTimerFunc(0, Timer, 0);
+	glutMouseFunc(OnMouseClick);
 	glutMainLoop();
 	
 	return 0;
