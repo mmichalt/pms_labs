a5835890963fc1c3372bfcfda36e2a2581ecaedc
Michal Matviychuk <michalmatviychuk@gmail.com>
2017-03-10 06:17:33 +0200

    Added lab3.1 done via Tao Framework in C#

--- lab3.1/OneMoreTime/Form1.Designer.cs
+++ lab3.1/OneMoreTime/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // sky
@@ -47,45 +50,70 @@
             this.sky.DepthBits = ((byte)(16));
             this.sky.Location = new System.Drawing.Point(4, 13);
             this.sky.Name = "sky";
-            this.sky.Size = new System.Drawing.Size(741, 382);
+            this.sky.Size = new System.Drawing.Size(826, 422);
             this.sky.StencilBits = ((byte)(0));
             this.sky.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(375, 412);
+            this.button1.Location = new System.Drawing.Point(12, 441);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Запустити";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(533, 412);
+            this.button2.Location = new System.Drawing.Point(105, 441);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(99, 23);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Призупинити";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // timer1
             // 
+            this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(302, 441);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(528, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Швидкість";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(831, 485);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.sky);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Лабораторна робота №3. Анімація";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,6 +123,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ lab3.1/OneMoreTime/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
+namespace OneMoreTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            sky.InitializeContexts();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Gl.glViewport(0, 0, sky.Width, sky.Height);
+            angle1 = 0;
+            gfPosX = .95;
+            gfPosY = .05;
+            linelength = .05;
+            red = Convert.ToSingle(130.0 / 255.0);
+            green = Convert.ToSingle(160.0 / 255.0);
+            blue = Convert.ToSingle(188.0 / 255.0);
+            Gl.glClearColor(red,green,blue,1);
+            rand=new Random();
+            starcount = rand.Next(10, 25);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000 - 95 * trackBar1.Value;
+            timer1.Start();
+        }
+
+        private double gfPosX, gfPosY,linelength;
+        private float red, green, blue;
+        private int starcount;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Призупинити")
+            {
+                timer1.Stop();
+                button2.Text = "Поновити";
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000 - 95 * trackBar1.Value;
+        }
+
+        private int c = 0;
+        private int angle1;
+        private Random rand;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+            if (angle1 != 0 && angle1 % 180 == 0)
+                c++;
+            if (c % 2 == 1)
+            {
+                Gl.glClearColor(0, 0, 0, 1);
+                Gl.glColor3f(1,1,1);
+                Gl.glBegin(Gl.GL_POINTS);
+                for (var i = 0; i < starcount; i++)
+                {
+                    Gl.glVertex2d(rand.NextDouble() * 2 - 1, rand.NextDouble()*2-1);
+                }
+                Gl.glEnd();
+            }
+            else
+                Gl.glClearColor(Convert.ToSingle(red), Convert.ToSingle(green), Convert.ToSingle(blue), 1);
+
+            Gl.glLoadIdentity();
+            Gl.glPushMatrix();
+            Gl.glTranslated(0.0, -1.0, .0);
+            Gl.glRotated(angle1, 0, 0,1);
+
+            Gl.glColor3d(1, 1, 1);
+            Gl.glBegin(Gl.GL_POLYGON);
+            Gl.glVertex2d(-1.2, -0.3);
+            Gl.glVertex2d(-1.15, -.1);
+            Gl.glVertex2d(-.97, 0.15);
+            Gl.glVertex2d(-.7,.15);
+            Gl.glVertex2d(-.95,.05);
+            Gl.glVertex2d(-1.1, -.1);
+            Gl.glVertex2d(-1.2, -0.3);
+            Gl.glEnd();
+
+            Gl.glColor3d(1.0, 1.0, 0.0);
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            Gl.glVertex2d(gfPosX, gfPosY);
+            Gl.glVertex2d(gfPosX - linelength, gfPosY - linelength);
+            Gl.glVertex2d(gfPosX, gfPosY);
+
+            Gl.glVertex2d(gfPosX + linelength, gfPosY);
+            Gl.glVertex2d(gfPosX + linelength, gfPosY - linelength);
+            Gl.glVertex2d(gfPosX + linelength, gfPosY);
+
+            Gl.glVertex2d(gfPosX + linelength*2.5, gfPosY);
+            Gl.glVertex2d(gfPosX + linelength*2.5 + linelength, gfPosY - linelength);
+            Gl.glVertex2d(gfPosX + linelength*2.5, gfPosY);
+
+            Gl.glVertex2d(gfPosX + linelength*2.5, gfPosY + linelength);
+            Gl.glVertex2d(gfPosX + linelength*2.5 + linelength, gfPosY + linelength);
+            Gl.glVertex2d(gfPosX + linelength*2.5, gfPosY + linelength);
+
+            Gl.glVertex2d(gfPosX + linelength*2.5, gfPosY + linelength*2.5);
+            Gl.glVertex2d(gfPosX + linelength*2.5 + linelength, gfPosY + linelength *2.5 + linelength);
+            Gl.glVertex2d(gfPosX + linelength *2.5, gfPosY + linelength *2.5);
+
+            Gl.glVertex2d(gfPosX + linelength, gfPosY + linelength *2.5);
+            Gl.glVertex2d(gfPosX + linelength, gfPosY + linelength *2.5 + linelength);
+            Gl.glVertex2d(gfPosX + linelength, gfPosY + linelength *2.5);
+
+            Gl.glVertex2d(gfPosX, gfPosY + linelength *2.5);
+            Gl.glVertex2d(gfPosX - linelength, gfPosY + linelength *2.5 + linelength);
+            Gl.glVertex2d(gfPosX, gfPosY + linelength *2.5);
+
+            Gl.glVertex2d(gfPosX, gfPosY + linelength);
+            Gl.glVertex2d(gfPosX - linelength, gfPosY + linelength);
+            Gl.glVertex2d(gfPosX, gfPosY + linelength);
+
+            Gl.glVertex2d(gfPosX, gfPosY);
+            Gl.glEnd();
+            Gl.glRectd(gfPosX, gfPosY, gfPosX + linelength *2.5, gfPosY + linelength *2.5);
+
+
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            sky.Invalidate();
+            angle1 += 10;
+        }
+    }
+}
